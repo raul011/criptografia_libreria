7fdf3523f7631f3a5ba1026d184617c64e216109
raul <raulalbertoaguilar01@gmail.com>
2026-04-30 19:49:17 -0400

    feat: restructure UI with tabs, simplify library imports (v1.0.4) and add technical docs

--- libreria/mi_crypto_lib/__init__.py
+++ libreria/mi_crypto_lib/__init__.py
@@ -9,3 +9,8 @@ Módulos disponibles:
 
 __version__ = "1.0.0"
 __author__ = "Raúl"
+
+# Exponemos las funciones principales
+from .teoria_numeros.crr import crr as calcular_crr
+from .teoria_numeros.phi import phi
+from .teoria_numeros.ejercicio_rsa_base import generar_primo_seguro
